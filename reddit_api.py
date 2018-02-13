3e9b92f4da9863d00fbaee905a20981cf615c5b4
DayGitH <akber91@gmail.com>
2018-02-13 14:28:59 -0800

    fixed type

--- reddit_api.py
+++ reddit_api.py
@@ -1,5 +1,5 @@
 client_id = 
 client_secret = 
 username = 'Day_Old_Pizza'
-user_agent = 'daily_programmer extractor (by /u/Day_Old_Pizza'
+user_agent = 'daily_programmer extractor (by /u/Day_Old_Pizza)'
 password = 
